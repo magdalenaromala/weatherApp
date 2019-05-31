7216214224d7b32efdaefe4c63830116394d4e9d
magda <magda17095@wp.pl>
2019-05-31 11:29:16 +0200

    add fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,49 @@
-import React, { Component } from 'react';
-import Form from './Form';
-import Result from './Result';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./Form";
+import Result from "./Result";
+import "./App.css";
 
 // Klucz do API
-const APIKey = 'eda12deaf7e86bb6d8f1e78fb9da554e'
+const APIKey = "eda12deaf7e86bb6d8f1e78fb9da554e";
 
 class App extends Component {
-
   state = {
-    value: '',
-    date: '',
-    city: '',
-    sunrise: '',
-    sunset: '',
-    temp: '',
-    pressure: '',
-    wind: '',
-    err: false,
-  }
+    value: "",
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    maine: "",
+    description: "",
+    icon: "",
+    wind: "",
+    err: false
+  };
 
-handleInputChange=(e)=>{
-  this.setState({
-    value:e.target.value
+  handleInputChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
 
-  })
-}
+  handleCitySubmit = e => {
+    e.preventDefault();
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${
+      this.state.value
+    }&APPID=${APIKey}&units=metric`;
 
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response;
+        }
+        throw Error("Nie udało się");
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Form = props => {
   return (
-    <form>
-      <input type="text" value={props.value}  onChange={props.change} placeholder="Wpisz miasto" />
+    <form onSubmit={props.submit}>
+      <input type="text" value={props.value}  onChange={props.change}  placeholder="Wpisz miasto" />
       <button>Wyszukaj miasta</button>
     </form>
   );
